98a442d2ccefa3aa3ea2f91e86f68e1c6aca2753
Chd Saglam <cihadsaglam@Chds-MacBook-Pro-2.local>
2018-04-08 22:05:19 +0200

    Node Callback Hell and Promises fixed problem

--- index.js
+++ index.js
@@ -17,10 +17,11 @@ MongoClient.connect(url).then((db) => {
             return dboper.findDocuments(db, "dishes");
         })
         .then((docs) => {
-                console.log("Found Documents:\n", docs);
+            console.log("Found Documents:\n", docs);
 
             return dboper.updateDocument(db, { name: "Vadonut" },
                                              { description: "Updated Test" }, "dishes");
+
         })
         .then((result) => {
             console.log("Updated Document:\n", result.result);
@@ -31,12 +32,13 @@ MongoClient.connect(url).then((db) => {
             console.log("Found Updated Documents:\n", docs);
                             
             return db.dropCollection("dishes");
-        })    
+        })
         .then((result) => {
             console.log("Dropped Collection: ", result);
 
             return db.close();
         })
         .catch((err) => console.log(err));
-}, (err) => console.log(err))
-   .catch((err) => console.log(err));
+
+})
+.catch((err) => console.log(err));
